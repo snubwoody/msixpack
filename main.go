ddc25f887e90ec7fa03263f2fbc3cc105ffdb213
waku <wakunguma13@gmail.com>
2025-11-04 12:07:14 -0700

    refactor: remove old unused commands

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"github.com/urfave/cli/v3"
 	"msixpack/cmd"
 	"os"
-	"os/exec"
 )
 
 func main() {
@@ -17,7 +16,6 @@ func main() {
 		Name:  "msixpack",
 		Usage: "CLI for packaging msix applications",
 		Commands: []*cli.Command{
-			bundleCmd(),
 			cmd.Create(),
 			cmd.Pack(),
 		},
@@ -27,24 +25,4 @@ func main() {
 		fmt.Printf("Error %s\n", err)
 		os.Exit(1)
 	}
-}
-
-func bundleCmd() *cli.Command {
-	cmd := &cli.Command{
-		Name:  "bundle",
-		Usage: "Bundle an msix application",
-		Action: func(ctx context.Context, command *cli.Command) error {
-			fmt.Println("Bundling app")
-			cmd := exec.Command("./.msixpack/windows-toolkit/makeappx.exe", "pack", "/d", "msix", "/p", "out.msix", "/o")
-			output, err := cmd.Output()
-			if err != nil {
-				fmt.Printf("Error: %s\n", err)
-				fmt.Printf("Output: %s\n", output)
-				return err
-			}
-			return nil
-		},
-	}
-
-	return cmd
 }
